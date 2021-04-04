51cd29b2679a3728ddd21502dafc814c92341c91
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 15:04:19 +0100

    Update: GetUser Method

--- src/controller/user/getUser.js
+++ src/controller/user/getUser.js
@@ -1,9 +1,13 @@
-async function getUser(req, res) {
+const User = require("../../models/user/model");
+
+async function getUser(req, res, next) {
+  const { id } = req.params;
+
   try {
-    const user = await User.find({_id: id}).lean();
+    const user = await User.find({ _id: id }).lean();
     res.send({ user });
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    next(err);
   }
 }
 
