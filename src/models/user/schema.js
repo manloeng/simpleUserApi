d596fa2809dc7e14e917ebf32b8b83f3aaf054a3
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 14:28:01 +0100

    Update Schema

--- src/models/user/schema.js
+++ src/models/user/schema.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let Users = new Schema(
   {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
     givenName: {
       type: String,
     },
     familyName: {
-      type: String,
-    },
-    email: {
       type: String,
     },
   },
