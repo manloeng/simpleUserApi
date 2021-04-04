705b4aae75ce5981f15bc72a530257da8412054f
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 14:57:24 +0100

    Add new GetAllusers method

--- src/controller/user/index.js
+++ src/controller/user/index.js
@@ -2,5 +2,6 @@ const createUser = require("./createUser");
 const deleteUser = require("./deleteUser");
 const getUser = require("./getUser");
 const updateUserByUserId = require("./updateUserByUserId");
+const getAllUsers = require("./getAllUsers");
 
-module.exports = { createUser, deleteUser, getUser, updateUserByUserId };
+module.exports = { createUser, deleteUser, getUser, updateUserByUserId, getAllUsers };
--- src/router/user.js
+++ src/router/user.js
@@ -1,7 +1,10 @@
 const userRouter = require("express").Router();
-const { createUser, deleteUser, getUser, updateUserByUserId } = require("../controller/user");
+const { createUser, deleteUser, getUser, updateUserByUserId, getAllUsers } = require("../controller/user");
 const { methodNotAllowed } = require("../error");
 
 userRouter.route("/").get(getUser).post(createUser).patch(updateUserByUserId).delete(deleteUser).all(methodNotAllowed);
+
+// Easy checks can be made here!
+userRouter.route("/all").get(getAllUsers).all(methodNotAllowed);
 
 module.exports = userRouter;
