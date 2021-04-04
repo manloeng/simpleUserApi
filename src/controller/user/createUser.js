f8f27ad7eb305694ed615a2d2e35328394546ba3
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 14:48:58 +0100

    Update Err Handling

--- src/controller/user/createUser.js
+++ src/controller/user/createUser.js
@@ -4,18 +4,18 @@ const { validateEmail } = require("../../utils/validator");
 async function createUser(req, res, next) {
   const { email, givenName, familyName } = req.body;
 
-  const emailExist = await User.exists({ email });
-  if (emailExist) next({ status: 400, msg: "Email Exists - please try user another email" });
-
-  const isEmailValid = validateEmail(email);
-  if (!isEmailValid) next({ status: 400, msg: "Please enter a valid email" });
-
-  // removed because people have started using numbers in their names
-  // const isGivenNameValid = validateString(givenName);
-  // const isFamilyNameValid = validateString(familyName);
-  // if (!isGivenNameValid || !isFamilyNameValid) next({ status: 400, msg: "Please enter a valid name" });
-
   try {
+    const emailExist = await User.exists({ email });
+    if (emailExist) throw new Error(next({ status: 400, msg: "Email Exists - please try user another email" }));
+
+    const isEmailValid = validateEmail(email);
+    if (!isEmailValid) throw new Error(next({ status: 400, msg: "Please enter a valid email" }));
+
+    // removed because people have started using numbers in their names
+    // const isGivenNameValid = validateString(givenName);
+    // const isFamilyNameValid = validateString(familyName);
+    // if (!isGivenNameValid || !isFamilyNameValid) next({ status: 400, msg: "Please enter a valid name" });
+
     const user = new User({
       email,
       givenName,
@@ -24,8 +24,8 @@ async function createUser(req, res, next) {
 
     user.save();
     res.send({ user });
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    next(err);
   }
 }
 
