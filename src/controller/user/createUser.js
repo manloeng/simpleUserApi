4137832fc95f6794c060290fc9c9a90b7558e965
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 14:28:37 +0100

    Add error handling to code

--- src/controller/user/createUser.js
+++ src/controller/user/createUser.js
@@ -1,5 +1,12 @@
+const validateEmail = require("../../utils/emailValidator");
+
 async function createUser(req, res, next) {
   const { email, givenName, familyName } = req.body;
+  const emailExist = await User.exists({ email });
+  if (!emailExist) next({ status: 400, msg: "Email Exists - please try user another email" });
+
+  const isEmailValid = validateEmail(email);
+  if (!isEmailValid) next({ status: 400, msg: "Please enter a valid email" });
 
   try {
     const user = new User({
