e6fd6939f8488a60a4432fafd72ac29d112a3cf8
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 14:13:30 +0100

    Create User Happy path

--- src/controller/user/createUser.js
+++ src/controller/user/createUser.js
@@ -1,5 +1,14 @@
-async function createUser(req, res) {
+async function createUser(req, res, next) {
+  const { email, givenName, familyName } = req.body;
+
   try {
+    const user = new User({
+      email,
+      givenName,
+      familyName,
+    });
+
+    res.send({ user });
   } catch (e) {
     console.log(e);
   }
