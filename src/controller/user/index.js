8e68696acb96ff339272b07ac1d6a36aa53ee84d
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 13:57:55 +0100

    Update routing

---
+++ src/controller/user/index.js
@@ -0,0 +1,6 @@
+const createUser = require("./createUser");
+const deleteUser = require("./deleteUser");
+const getUser = require("./getUser");
+const updateUserByUserId = require("./updateUserByUserId");
+
+module.exports = { createUser, deleteUser, getUser, updateUserByUserId };
--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,7 @@
 const userRouter = require("express").Router();
+const { createUser, deleteUser, getUser, updateUserByUserId } = require("../controller/user");
 const { methodNotAllowed } = require("../error");
 
-// userRouter.route("/").get().post().update().delete().all(methodNotAllowed);
+userRouter.route("/").get(getUser).post(createUser).patch(updateUserByUserId).delete(deleteUser).all(methodNotAllowed);
 
 module.exports = userRouter;
