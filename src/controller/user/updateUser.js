b5d34ff196f5cb6d69ba3047ad1ccc1ca851ed01
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 20:52:56 +0100

    Update UpdateUser Method

--- src/controller/user/updateUser.js
+++ src/controller/user/updateUser.js
@@ -1,7 +1,20 @@
-async function updateUser(req, res) {
+const User = require("../../models/user/model");
+
+async function updateUser(req, res, next) {
+  const { id } = req.params;
+  const data = req.body;
+
   try {
-  } catch (e) {
-    console.log(e);
+    const userExist = await User.exists({ _id: id });
+    if (!userExist) throw new Error(next({ status: 400, msg: "User Doesn't exist" }));
+
+    const user = await User.findByIdAndUpdate({ _id: id }, { $set: data }, { new: true }).exec();
+
+    if (user) {
+      return res.status(200).json(user);
+    }
+  } catch (err) {
+    next(err);
   }
 }
 
