ac7e5668354e85a8a6cec951261545612c774def
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 22:13:58 +0100

    Update: User Cannot be deleted on route /users

--- src/router/user.js
+++ src/router/user.js
@@ -2,7 +2,7 @@ const userRouter = require("express").Router();
 const { createUser, deleteUser, getUser, updateUser, getAllUsers } = require("../controller/user");
 const { methodNotAllowed } = require("../error");
 
-userRouter.route("/").get(getAllUsers).post(createUser).delete(deleteUser).all(methodNotAllowed);
+userRouter.route("/").get(getAllUsers).post(createUser).all(methodNotAllowed);
 userRouter.route("/:id").get(getUser).post(createUser).patch(updateUser).delete(deleteUser).all(methodNotAllowed);
 
 module.exports = userRouter;
