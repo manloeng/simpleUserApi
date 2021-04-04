5d00069a9ef02bab65dd42525064585a6de9381c
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 20:47:27 +0100

    Add error handling to delete req

--- src/controller/user/deleteUser.js
+++ src/controller/user/deleteUser.js
@@ -3,6 +3,9 @@ const User = require("../../models/user/model");
 async function deleteUser(req, res, next) {
   const { id } = req.params;
   try {
+    const userExist = await User.exists({ _id: id });
+    if (!userExist) throw new Error(next({ status: 400, msg: "User Doesn't exist" }));
+
     await User.findOneAndDelete({ _id: id }).exec();
     res.send({ msg: "Sucessfully Deleted" });
   } catch (err) {
