0d5afa0fe0281a12dd679b9e9ffafe8cf65533cb
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 20:45:43 +0100

    Update Delete Method

--- src/controller/user/deleteUser.js
+++ src/controller/user/deleteUser.js
@@ -1,7 +1,12 @@
-async function deleteUser(req, res) {
+const User = require("../../models/user/model");
+
+async function deleteUser(req, res, next) {
+  const { id } = req.params;
   try {
-  } catch (e) {
-    console.log(e);
+    await User.findOneAndDelete({ _id: id }).exec();
+    res.send({ msg: "Sucessfully Deleted" });
+  } catch (err) {
+    next(err);
   }
 }
 
