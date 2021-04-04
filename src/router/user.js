1d71bb884a755b8597fcf83d7a645d8a30b1d7c4
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-04 20:46:09 +0100

    Update: Routing

--- src/router/user.js
+++ src/router/user.js
@@ -2,10 +2,7 @@ const userRouter = require("express").Router();
 const { createUser, deleteUser, getUser, updateUser, getAllUsers } = require("../controller/user");
 const { methodNotAllowed } = require("../error");
 
-userRouter.route("/").post(createUser).delete(deleteUser).all(methodNotAllowed);
+userRouter.route("/").get(getAllUsers).post(createUser).delete(deleteUser).all(methodNotAllowed);
 userRouter.route("/:id").get(getUser).post(createUser).patch(updateUser).delete(deleteUser).all(methodNotAllowed);
-
-// Easy checks can be made here!
-userRouter.route("/all").get(getAllUsers).all(methodNotAllowed);
 
 module.exports = userRouter;
