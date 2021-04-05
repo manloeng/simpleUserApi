1ba7369011521954693e49c2360fb3b3cfa039c9
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-05 10:53:53 +0100

    Update: Returning 204 - returns no content

--- src/controller/user/deleteUser.js
+++ src/controller/user/deleteUser.js
@@ -7,7 +7,7 @@ async function deleteUser(req, res, next) {
     if (!userExist) throw new Error(next({ status: 404, msg: "User Doesn't exist" }));
 
     await User.findOneAndDelete({ _id: id }).exec();
-    res.status(204).send({ msg: "Sucessfully Deleted" });
+    res.status(204);
   } catch (err) {
     next(err);
   }
